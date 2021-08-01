eec64d2f3c578b413d4dd767b0f23eaa68b8eb3a
HB-1001 <75037684+HB-1001@users.noreply.github.com>
2021-08-01 12:13:04 +0400

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import { Provider } from "next-auth/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }) {
-    
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Provider session={pageProps.session}>
       <ChakraProvider>
