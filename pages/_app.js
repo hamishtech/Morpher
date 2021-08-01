0bb64560e2eec6b103ded26c43925f8053adb0f8
HB-1001 <75037684+HB-1001@users.noreply.github.com>
2021-08-01 12:27:58 +0400

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import { Provider } from "next-auth/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Provider session={pageProps.session}>
       <ChakraProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,20 @@ export default class Document extends NextDocument {
             data-domain='morpher.vercel.app'
             src='https://plausible.io/js/plausible.js'
           ></script>
+          <script
+            async
+            src='https://www.googletagmanager.com/gtag/js?id=G-Y150EK0J4N'
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Y150EK0J4N');
+            `,
+            }}
+          />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
